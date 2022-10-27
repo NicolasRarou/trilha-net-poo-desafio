02953833e566c394cba08b5f5c883438e69900d9
Nicolas Einstein <nicolaseins.cs@gmail.com>
2022-10-27 00:32:28 -0300

    Implemetações feitas, classe Nokia e Iphone, agoram herdam de smartphone, seus construtores foram feitos como pedido e program.cs agora escreve o recebimento de ligações e instalação de aplicativos para objetos criados apartir da classe Nokia e Iphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+
+Nokia nokiaG1 = new Nokia(numero: "1234567", modelo: "Modelo 2", imei: "9999999999", memoria: 64);
+nokiaG1.Ligar();
+nokiaG1.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n");
+
+
+Console.WriteLine("Smartphone Iphone");
+Iphone iphone30g = new Iphone(numero: "987654321", modelo: "Modelo G-40", imei: "11111111111", memoria: 98);
+iphone30g.ReceberLigacao();
+iphone30g.InstalarAplicativo("Telegram");
